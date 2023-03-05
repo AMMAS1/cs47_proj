d3a7dfa40e2ee060d9dc924bfc284428c48337f7
AMMAS1 <ammas@stanford.edu>
2023-03-05 15:07:56 -0800

    added the navigation

--- App.js
+++ App.js
@@ -1,104 +1,45 @@
-import { Pressable, SafeAreaView, StyleSheet, Text, View } from "react-native";
-import { LineChart } from "react-native-chart-kit";
-import { Dimensions } from "react-native";
-import React, { useEffect, useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import MainP from "./screens/mainP";
+import Rules from "./screens/rulesP";
+import ProfileP from "./screens/profileP";
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
-  const sma = 5;
-  const [data, setData] = useState({
-    labels: ["January", "February", "March", "April", "May", "June"],
-    datasets: [
-      {
-        data: [
-          Math.random() * 100,
-          Math.random() * 100,
-        ],
-      },
-      {
-        data: [
-          Math.random() * 100,
-          Math.random() * 100,
-        ],
-      },
-      {
-        data: [
-          Math.random() * 100,
-          Math.random() * 100,
-        ],
-      },
-    ],
-  });
-  const [text, setText] = useState("O");
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setData({
-        ...data,
-        datasets: [
-          {
-            data: [
-              ...data.datasets[0].data.slice(
-                data.datasets[0].data.length == 20 ? 1 : 0
-              ),
-              Math.random() * 100,
-            ],
-          },
-          {
-            color: (opacity = 1) => `rgba(255, 0, 255, ${opacity})`,
-            data: Array(data.datasets[0].data.length).fill(
-              data.datasets[0].data.reduce((a, b) => a + b) /
-                data.datasets[0].data.length)
-            ,
-          },
-          {
-            color: (opacity = 1) => `rgba(0, 255, 0, ${opacity})`,
-            data: Array(data.datasets[0].data.length).fill(
-              data.datasets[0].data.slice(data.datasets[0].data.length - sma).reduce((a, b) => a + b) /
-              sma)
-            ,
-          },
-        ],
-      });
-      setText(data.datasets[0].data.slice(data.datasets[0].data.length - sma).reduce((a, b) => a + b) / sma < data.datasets[0].data.reduce((a, b) => a + b) / data.datasets[0].data.length ? "Buy" : "Sell"); 
-    }, 1);
-    return () => clearInterval(interval);
-  }, [data]);
-
   return (
-    <SafeAreaView>
-      <View>
-        <LineChart
-          data={data}
-          width={Dimensions.get("window").width} // from react-native
-          height={220}
-          yAxisLabel="$"
-          yAxisSuffix="k"
-          yAxisInterval={1} // optional, defaults to 1
-          chartConfig={{
-            backgroundColor: "#e26a00",
-            backgroundGradientFrom: "#fb8c00",
-            backgroundGradientTo: "#ffa726",
-            decimalPlaces: 2, // optional, defaults to 2dp
-            color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-            labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-            style: {
-              borderRadius: 16,
-            },
-            propsForDots: {
-              r: "0",
-              strokeWidth: "2",
-              stroke: "#ffa726",
-            },
-          }}
-          bezier
-          style={{
-            marginVertical: 8,
-            borderRadius: 16,
-          }}
-        />
-        <Text>{text}</Text>
-      </View>
-    </SafeAreaView>
-  );
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'Main') {
+              iconName = focused
+                ? 'home'
+                : 'home-outline';
+            } else if (route.name === 'Rules') {
+              iconName = focused ? 'ios-list' : 'ios-list-outline';
+            }
+            else if (route.name === 'Profile') {
+              iconName = focused ? 'person' : 'person-outline';
+            }
+
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: 'tomato',
+          tabBarInactiveTintColor: 'gray',
+        })}
+      >
+        <Tab.Screen name="Main" component={MainP} />
+        <Tab.Screen name="Rules" component={Rules} />
+        <Tab.Screen name="Profile" component={ProfileP} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  )
 }
 
 const styles = StyleSheet.create({
